ef92a7306e3b848f7ca77b3d3b71729cd1ae8cf4
yalh76 <yalh@yahoo.com>
2021-03-10 03:22:07 +0100

    First test of ynh_install_go

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="git"
 
 #=================================================
 # PERSONAL HELPERS
