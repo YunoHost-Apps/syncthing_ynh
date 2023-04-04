85455c5bc77d7a26e544e3b236bd2a024a22cc7f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-04 09:37:45 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies=""
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
